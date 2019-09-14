848c7deab3af052877cd2d04f63111740e891e52
unknown <michal.weis87@gmail.com>
2019-09-14 20:13:25 +0200

    zadanie 3

---
+++ zad3.py
@@ -0,0 +1,16 @@
+# Zadanie 3
+# Napisz skrypt, który pozwala wprowadzić 4 liczby calkowite, dodać je i wyświetl wynik na ekranie.
+# Wejście i wyjście powinny być zrozumiałe dla użytkownika.
+'''
+print("------------------ Sumowanie liczb ------------------")
+print("Program pyta użytkownika o 4 dowolne liczby całkowite")
+print("a następnie zwraca ich sumę.")
+print()
+'''
+
+a = int(input("Podaj liczbę całkowitą no 1: "))
+b = int(input("Podaj liczbę całkowitą no 2: "))
+c = int(input("Podaj liczbę całkowitą no 3: "))
+d = int(input("Podaj liczbę całkowitą no 4: "))
+
+print("Suma wyżej wymienionych liczb całowitych to: ", a + b + c + d)
