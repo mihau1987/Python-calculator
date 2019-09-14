23e4bdd094f8ae77eb6bbce13c2f7a662d651162
unknown <michal.weis87@gmail.com>
2019-09-14 19:45:25 +0200

    zadania

---
+++ zad1.py
@@ -0,0 +1,12 @@
+# Zadanie 1
+# Napisz skrypt, który oblicza z jaką siłą przyciąga cię ziemia,
+# z jaką siłą przyciągałby Cię księżyc
+
+print("Z jaką siłą przyciąga Cię ziemia")
+f = int(input("Podaj swoją wagę: "))
+
+print("Ziemia przyciąga Cię z siłą: ", f * 10,"N")
+
+
+
+
---
+++ zad2.py
@@ -0,0 +1,9 @@
+# Zadanie 2
+# Napisz skrypt, który pobiera dwie wiadomości od użytkownika
+# a następnie wyświetla je na ekranie poprzedzone ostrzeżeniem "UWAGA: ..." ​
+a = input("Podaj swoje imię: ")
+b = int(input("Podaj swój wiek: "))
+print("UWAGA: ", a, b)
+
+
+
