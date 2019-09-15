b4592eb891a8556d63606d7dea014beef07dff35
unknown <michal.weis87@gmail.com>
2019-09-15 10:20:46 +0200

    ex5

---
+++ zad5.py
@@ -0,0 +1,14 @@
+# Zadanie 5
+# Napisz program, który pyta użytkownika o 2 liczby
+# a następnie dzieli jedna przez drugą.
+# Pokaż ile razy pierwsza liczba mieści się w drugiej
+# oraz jaka jest reszta tego dzielenia. 
+
+a = int(input("Podaj liczbę całkowitą no 1: "))
+b = int(input("Podaj liczbę całkowitą no 2: "))
+
+print(a / b)
+
+print(a // b)
+
+print(a % b)
