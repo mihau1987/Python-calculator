c1ffa48b4cd73c8e169f7e94ef506f68bc77c057
unknown <michal.weis87@gmail.com>
2019-09-15 10:12:14 +0200

    ex4

---
+++ zad4.py
@@ -0,0 +1,12 @@
+# Zadanie 4
+# Napisz skrypt, który zapyta użytkownika o trzy liczby całkowite,
+# a następnie pomnóż dwa pierwsze. przed podzieleniem wyniku przez trzecią liczbę. 
+# Wejście i wyjście powinny być zrozumiałe dla użytkownika.
+
+a = int(input("Podaj liczbę całkowitą no 1: "))
+b = int(input("Podaj liczbę całkowitą no 2: "))
+c = int(input("Podaj liczbę całkowitą no 3: "))
+
+print((a * b) / c)
+
+exit()
