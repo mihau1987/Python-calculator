bbe11e4d4939d094bf632632c0a8d8cd467c42fe
unknown <michal.weis87@gmail.com>
2019-09-16 18:59:09 +0200

    Exercise

--- Zmienne_typy2/strings/zad3.py
+++ Zmienne_typy2/strings/zad3.py
@@ -13,3 +13,15 @@
 quote = "Honesty is the first chapter in the book of wisdom."
 
 print(len(quote))
+
+print(quote[-7:])
+
+print(quote[quote.find(".")])
+
+print(quote[25:51:3])
+
+print(quote[0:51:2])
+
+print(quote[::-1])
+
+print(quote.replace("wisdom", "friendship"))
