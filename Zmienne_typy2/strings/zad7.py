4db090e7719fce075cf3ebc1583cf44438e307d8
unknown <michal.weis87@gmail.com>
2019-09-19 16:26:40 +0200

    Exercises

--- Zmienne_typy2/strings/zad7.py
+++ Zmienne_typy2/strings/zad7.py
@@ -1,3 +1,23 @@
 # Zadanie 7
 # Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową.
 # Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
+
+
+
+txt = "The end of {0:s} was in year{1: d}"
+
+print(txt.format("WWII", 1945))
+
+
+# Powyżej - zrobiłem to tak jak pamiętałem z udemy, wiem że to niezgodne z poleceniem
+
+
+txt = "The end of {0:s} was in year{1: d}"
+
+num = 1945
+
+print(txt.format("WWII", num))
+
+#Oho wpadłem w międzyczasie jeszcze na powyższe rozwiązanie, chyba to jest bardziej zgodne z poleceniem, yes?
+
+
--- Zmienne_typy2/zad5.py
+++ Zmienne_typy2/zad5.py
@@ -6,3 +6,8 @@ if (4%2 == 0):
     print(True)
 else:
     print(False)
+
+
+# lub prostszy wariant, czyli po prostu:
+
+print(4%2 == 0)
