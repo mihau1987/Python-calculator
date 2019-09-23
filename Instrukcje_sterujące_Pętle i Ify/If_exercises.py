57504d79fe20f1ab1d671e198d6093733d3061be
unknown <michal.weis87@gmail.com>
2019-09-23 14:48:52 +0200

    Exercises

---
+++ Instrukcje_sterujące_Pętle i Ify/If_exercises.py
@@ -0,0 +1,8 @@
+#1. Sprawdź czy liczba podana przez użytkownika jest podzielna przez 3 bez reszty.
+a = int(input("Podaj liczbę: "))
+
+if a%3 == 0:
+    print("Liczba jest podzielna przez 3 bez reszty")
+else:
+    print("Podana liczba nie jest podzielna przez 3 bez reszty")
+
