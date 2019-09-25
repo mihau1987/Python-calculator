c3fa7e8fe78c57cf9b959b21f53f7f2ce816abaa
unknown <michal.weis87@gmail.com>
2019-09-25 18:47:54 +0200

    Exercise

---
+++ Instrukcje_sterujące_Pętle i Ify/8_If_exercise.py
@@ -0,0 +1,19 @@
+#Sortowanie.
+#Trzy dowolne liczby podane przez użytkownika zapisz do trzech zmiennych.
+
+#Znajdź największą liczbę??????
+
+#Wyświetl liczby od największej do najmniejszej.
+
+a = int(input("Podaj liczbę a: "))
+b = int(input("Podaj liczbę b: "))
+c = int(input("Podaj liczbę c: "))
+
+lista = [a, b, c]
+
+print(lista)
+
+lista.sort()
+lista.reverse()
+
+print("Wyżej wymienione liczby w kolejności od największej do najmniejszej:","\n",lista)
