926ae13564921b8b12c64c6203d9921ee526e77d
unknown <michal.weis87@gmail.com>
2019-10-09 23:04:52 +0200

    Exercises

--- Trello_Zadania/Zmienne_typy2/zad1.py
+++ Trello_Zadania/Zmienne_typy2/zad1.py
@@ -3,3 +3,20 @@
 # Załóż, że spalanie na 100km wynosi 6,4 l,
 # trasa to 120km,
 # litr benzyny kosztuje 5,04 zł.
+
+spalanie_100 = 6.4
+
+trasa = 120
+
+cena_benz = 5.04
+
+print("Przy trasie 120 km spalanie wynosi:",(trasa * spalanie_100)/100)
+
+print("Całkowity koszt wyprawy to:",(trasa * spalanie_100)/100 * 5.04) 
+
+
+
+
+
+
+
