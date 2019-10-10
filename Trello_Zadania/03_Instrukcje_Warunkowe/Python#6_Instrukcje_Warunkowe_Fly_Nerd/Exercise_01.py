5d546979957d45e4e76f9bc351032a8fdcafda3a
unknown <michal.weis87@gmail.com>
2019-10-10 09:22:13 +0200

    Exercises

--- Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_01.py
+++ Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_01.py
@@ -1,8 +1,16 @@
+#Zadanie 1
+#Skrypt zapyta użytkownika o wiek. Jeżeli użytkownik jest przed 18 wyświetli informację „Użytkownik niepełnoletni” oraz zwróci ile lat zostało użytkownikowi do pełnoletności.
+#Użytkownikom pełnoletnim wyświetli informację „Użytkownik pełnoletni”.
+#Sprawdź czy wiek użytkownika nie przekracza 100 lat i wyświetl komunikat „200 lat ♫”.
+
+
 age = int(input("Podaj swój wiek: "))
 
 if age > 18 and age < 100:
     print("Użytkownik pełnoletni")
 elif age < 18:
     print("Użytkownik niepełnoletni, do pełnoletności zostało Ci:", 18 - age)
-elif age > 100:
+elif age > 100 and age <= 200:
     print("200 lat .|-.|")
+else:
+    print("Wolne żarty ;)")
---
+++ Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_02.py
@@ -0,0 +1,8 @@
+wzrost = float(input("Podaj swoj wzrost: "))
+
+waga = float(input("Podaj swoją wagę: "))
+
+BMI = waga/(wzrost**2)
+
+print("Twoje BMI wynosi:", BMI)
+
