969124b92f5d32ba262eb8da004509cea8dfd5b7
unknown <michal.weis87@gmail.com>
2019-10-10 08:43:03 +0200

    Exercises

--- Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_01.py
+++ Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_01.py
@@ -3,6 +3,6 @@ age = int(input("Podaj swój wiek: "))
 if age > 18 and age < 100:
     print("Użytkownik pełnoletni")
 elif age < 18:
-    print("Użytkownik niepełnoletni")
+    print("Użytkownik niepełnoletni, do pełnoletności zostało Ci:", 18 - age)
 elif age > 100:
     print("200 lat .|-.|")
