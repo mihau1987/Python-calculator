25fac1b56dcf16892f500962215c8354bad4a278
unknown <michal.weis87@gmail.com>
2019-10-10 08:18:25 +0200

    Exercises

--- Trello_Zadania/Zmienne_typy2/zad2.py
+++ Trello_Zadania/Zmienne_typy2/zad2.py
@@ -1,2 +1,14 @@
 # Zadanie 2
 # Zmodyfikuj skrypt zad1.py tak, by przyjmował wartości od użytkownika
+
+
+spalanie_100 = float(input("Podaj ilość litrów spalanych na godzinę: "))
+
+trasa = float(input("Podaj ilość km do przejechania: "))
+
+cena_benz = float(input("Podaj cenę benzyny: "))
+
+print("Przy trasie 120 km spalanie wynosi:",(trasa * spalanie_100)/100)
+
+print("Całkowity koszt wyprawy to:",(trasa * spalanie_100)/100 * 5.04) 
+
