b91bf6d7bb1120f9912981e2226848c08f7af5c2
unknown <michal.weis87@gmail.com>
2019-10-12 20:01:02 +0200

    Exercises

---
+++ Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_03.py
@@ -0,0 +1,18 @@
+x = 6
+
+y = 27
+
+z = 134
+
+if (x>y) and (x>z):
+    maximum = x
+elif (y>x) and (y>z):
+    maximum = y
+else:
+    maximum = z
+
+print(maximum)
+
+#Druga czesc do wspolnego przerobienia.
+
+#p.s. po kazdej lekcji warto odnotowac moje braki z poprzednich "rozdzialow" abym mogl to nadrobic :)
--- Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_04.py
+++ Trello_Zadania/03_Instrukcje_Warunkowe/Python#6_Instrukcje_Warunkowe_Fly_Nerd/Exercise_04.py
@@ -1,6 +1,8 @@
 # WORK IN PROGRESS
 
-fem_nam = ["Katarzyna", "Natalia", "Monika", "Agnieszka"]
+#1. Rozwiązanie za pomocą listy:
+
+'''fem_nam = ["Katarzyna", "Natalia", "Monika", "Agnieszka"]
 
 male_nam = ["Jan", "Adrian", "Wojtek", "Eryk"]
 
@@ -12,4 +14,32 @@ elif name in male_nam:
     print("This is male name")
 else:
     print("Name is not on the list")
+
+# Dalej niestety nie wiem'''
+
+#2. Rozwiązanie przy pomocy słownika:
+
+names = {"Kasia": "zenskie", "Ania": "zenskie", "Wiesia": "zenskie",
+         "Arnold": "meskie", "Ferdynand": "meskie", "Marian": "meskie"}
+
+name = input("Podaj imię: ")
+
+if (name in list(names.keys())):
+    print(name, "to imię", names[name])
+else:
+    print("Nie mamy tego imienia dodaj je do listy!")
+    
+    plec = input("To imię meskie czy zenskie? Wpisz m/z: ")
+    if (plec == "m"):
+        names[name] = "meskie"
+    else:
+        names[name] = "zenskie"
+
+print(list(names.keys()))
+
+
+
+
+
+
     
