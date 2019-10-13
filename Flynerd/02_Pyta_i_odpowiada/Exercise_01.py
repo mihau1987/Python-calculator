e3317d99ff9cbb155f69c92bb722c85aa7e0be11
unknown <michal.weis87@gmail.com>
2019-10-13 16:21:14 +0200

    Exercises

---
+++ Flynerd/02_Pyta_i_odpowiada/Exercise_01.py
@@ -0,0 +1,17 @@
+'''Zadanie 1
+Teraz chcemy, tak aby skrypt:
+
+Zapytał o Twoje imię
+Powitał Cię po imieniu
+Zapytał o Twój wiek
+Zapytał o peron z Harrego Potter’a
+Odpowiedział (dowolnie łącząc wiek i peron), czy jest sens jechać do Hogwartu ;)'''
+
+name = input("Jak masz na imię? ")
+
+print("Cześć", name,"Miło Cię poznać!")
+
+wiek = input("Ile masz lat? ")
+peron = input("Z którego peronu dostaniesz się do Hogwartu? ")
+
+print("W wieku",wiek, "lat, nie ma sensu jechać do Hogwartu z peronu", peron)
