11cdf6a736f3b6a3c9b1eeb3ee058f81971d1f98
unknown <michal.weis87@gmail.com>
2019-10-21 19:58:42 +0200

    Exercises

---
+++ Udemy/Kurs_Arek_Wlodarczyk/Petla_While.py
@@ -0,0 +1,23 @@
+'''liczba = 100
+
+while liczba >= 0:
+    print(liczba)
+    liczba -= 1
+'''
+wynik = 0
+
+i = 0
+
+while i < 4:
+    x = int(input("Podaj liczbe: "))
+    wynik += x
+    i += 1
+
+print("Wynik dodawania liczb to:", wynik)
+
+    
+    
+
+
+
+    
---
+++ YouTube/Pętla_While_Piotr_Baja.py
@@ -0,0 +1,33 @@
+print("Start")
+
+i = 0
+
+while i < 10:
+    i += 1
+    print(i)
+
+
+print("Koniec")
+
+
+i = 10
+
+while i > 0:
+    print(i)
+    i -= 1
+
+
+'''while True:
+    print("Wpisz liczbe")
+    x = input()
+    a = int(x)
+    print(a*a)
+'''
+
+suma = 0
+
+while True:
+    print("Wpisz liczbe")
+    x = input()
+    suma += int(x)
+    print(suma)
