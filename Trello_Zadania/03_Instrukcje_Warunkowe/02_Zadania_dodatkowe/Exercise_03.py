cf1dd62b55a85ce4e716f55e0cfe12e5f1d3829b
unknown <michal.weis87@gmail.com>
2019-10-27 13:25:07 +0100

    Exercises

--- Trello_Zadania/03_Instrukcje_Warunkowe/02_Zadania_dodatkowe/Exercise_03.py
+++ Trello_Zadania/03_Instrukcje_Warunkowe/02_Zadania_dodatkowe/Exercise_03.py
@@ -1,5 +1,34 @@
-BTC = 140
+'''
+Napisz konwerter walut.
+Użytkownik podaje jedną z walut kod literowy np USD, EUR, HUF, a następnie kwotę do wymiany.
+Podstawowa waluta to PLN. Wyświetl liczby zaokrąglone do 2 miejsc po przecinku.
+Konwerter powinien działać w 2 strony.
 
-BCH = 150
+Kiedy wymienisz 500 USD na PLN ile banknotów dostaniesz.
+Zakładamy, że chcesz jak najwięcej banknotów z każdego nominału od (100, z reszty liczysz 50 itd, 20, 10)
+'''
 
-ETC = 160
+
+currency = input("Which currency you like to convert the money into?: ")
+
+amount_PLN = float(input("Enter the amount of money in PLN you wish to convert: "))
+
+EUR = 4.27
+
+USD = 3.86
+
+HUF = 0.01
+
+
+if currency == "EUR":
+    print(round(amount_PLN * EUR,2))
+elif currency == "USD":
+    print(round(amount_PLN * USD,2))
+elif currency == "HUF":
+    print(round(amount_PLN * HUF,2))
+
+
+
+
+
+
